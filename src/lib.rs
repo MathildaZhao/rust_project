3d247961caa187a4ccbd6e740966379cb5b93b0b
MathildaZhao <jiyuan6@ualberta.ca>
2021-11-22 01:31:54 -0700

    some fns of AVL

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,1 @@
 pub mod AVLTree;
-
-
-// fn r_get_num_leaves(node: &Rc<RefCell<AVLNode<u32>>>, mut current_count: u32) -> u32 {
-//     let node = node.as_ref().borrow_mut();
-//     if let Some(left_node) = &node.left {
-//         current_count = r_get_num_leaves(left_node, current_count);
-//     }
-//     if let Some(right_node) = &node.right {
-//         current_count = r_get_num_leaves(right_node, current_count);
-//     }
-//     if node.left.is_none() && node.right.is_none() {
-//         current_count = current_count + 1;
-//     }
-//     current_count
-// }
