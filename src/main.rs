93d103b642feb0dbe64e8c0a85f687146bf17f76
MathildaZhao <jiyuan6@ualberta.ca>
2021-11-22 01:29:53 -0700

    some fns of AVL

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+use std::borrow::BorrowMut;
+
+mod AVLTree;
+
 fn main() {
-    println!("This is ece 522 project");
-    println!("hello!");
+    let mut TestNode = AVLTree::AVLTree::new();
+    for i in vec![3,5,6,8,2] {
+        TestNode.insert(i);
+    }
+    println!("{:#?}",TestNode);
+    println!("Is the searched value exist? The answer is {:?}.",TestNode.search(5));
+    println!("Is the searched value exist? The answer is {:?}.",TestNode.search(11));
+    println!("Height is: {}", TestNode.height());
+    println!("If the tree is empty? The answer is {}.", TestNode.is_empty());
 }
